21a81bf8e30379e74327eeeb81d2b4fb7c838610
NicoDVKa <nicodvkcss@gmail.com>
2023-04-28 10:24:45 -0300

    Add Config in AppDbContext

---
+++ Domain/Entities/Match.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domain.Entities
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public int User1Id { get; set; }
+        public int User2Id { get; set;}
+        public DateTime CreatedAt { get; set; }
+
+        public Date Date { get; set; }
+    }
+}
