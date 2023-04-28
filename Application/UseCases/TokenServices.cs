cac00fa065e61ddab9e5c981ced5b0779702dfb0
NicoDVKa <nicodvkcss@gmail.com>
2023-04-28 12:39:29 -0300

    Add Jwt Config

---
+++ Application/UseCases/TokenServices.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Application.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Application.UseCases
+{
+    public class TokenServices : ITokenServices
+    {
+        public bool IsExpiredToken(ClaimsIdentity identity)
+        {
+
+            var exp = identity.Claims.FirstOrDefault(x => x.Type == "exp").Value;
+
+            var expirationDateUnix = long.Parse(exp.ToString());
+            var expirationDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(expirationDateUnix);
+
+            return expirationDateTimeOffset.UtcDateTime > DateTime.UtcNow;
+
+        }
+
+        public bool ValidateUserId(ClaimsIdentity identity,int userId)
+        {
+            try
+            {
+                var id = identity.Claims.FirstOrDefault(x => x.Type == "UserId").Value;
+
+                if (id != userId.ToString())
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MatchMicroService/Controllers/MatchController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Application.Interfaces;
+using Microsoft.OpenApi.Models;
+
+namespace MatchMicroService.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        private readonly ITokenServices _tokenServices;
+
+        public MatchController(ITokenServices tokenServices) 
+        {   
+            _tokenServices = tokenServices;
+        }
+
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetMatchById(string id)
+        {
+            // Ejemplo de uso del token
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            _tokenServices.IsExpiredToken(identity);
+
+            // Obtener el id de los users del match
+            int userId = 0;
+            _tokenServices.ValidateUserId(identity, userId);
+
+            return Ok();
+        }
+    }
+}
