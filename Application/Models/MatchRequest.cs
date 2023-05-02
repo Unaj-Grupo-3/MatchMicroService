eae1cea7711867bfd5a6c399f681fcfc3c683588
NicoHerlan <nicodvkcss@gmail.com>
2023-05-02 06:51:25 -0300

    Create models

---
+++ Application/Models/MatchRequest.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.Models
+{
+    public class MatchRequest
+    {
+        public int User1 { get; set; }
+        public int User2 { get; set; }
+    }
+}
---
+++ Application/Models/MatchResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Application.Models
+{
+    public class MatchResponse
+    {
+        public int Id { get; set; }
+        public int User1 { get; set; }
+        public int User2 { get; set; }
+    }
+}
