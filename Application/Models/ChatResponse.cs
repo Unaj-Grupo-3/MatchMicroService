fdd6302a56a5968ff98bd863174e4041778db7e9
gosapp <gosapps86contact@gmail.com>
2023-05-07 12:48:50 -0300

    act

---
+++ Application/Models/ChatResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class ChatResponse
+    {
+        public int User2Id { get; set; }
+        public int User1Id { get; set; }
+        public int ChatId { get; set; }
+    }
+}
